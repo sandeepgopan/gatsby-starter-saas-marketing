0973da94ce9691ed7071cf29924a27c08a820551
Sandeep Gopan <sandeepgopan111@gmail.com>
2020-05-03 22:26:10 +0530

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import React from "react"
 import Layout from "../components/common/layout/layout"
 import SEO from "../components/common/layout/seo"
 import Navigation from "../components/common/navigation/navigation"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 
 import Header from "../components/sections/header"
 import Features from "../components/sections/features"
@@ -14,6 +17,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Navigation />
     <Header />
+    <h1><GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  /></h1>
+    
     <Features />
     <GetStarted />
     <Footer />
